df1f65dbab4dabf8c81b3fd1cc74d034e79d3eea
Vaibhav Rokde <Vaibhavrokde14@gmail.com>
2024-08-06 10:45:45 +0530

    Adding API-GateWay

---
+++ api-gatway/src/test/java/com/practice/onlinestore/apigetway/ApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.onlinestore.apigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
